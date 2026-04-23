adf99d4b26e2777bffb3e4a044815abcd3ac8ad7
ignacio <ignacio.delacruz@vonage.com>
2026-04-23 10:36:54 +0200

    fix(android): remove prepare script copy that causes duplicate class error

--- src/generated/packageInfo.js
+++ src/generated/packageInfo.js
@@ -3,6 +3,6 @@
 // Do not edit manually.
 
 export const OTRN_PACKAGE_INFO = {
-  "version": "2.32.1",
+  "version": "2.33.0",
   "repositoryUrl": "https://github.com/Vonage/vonage-video-react-native-sdk.git"
 };
