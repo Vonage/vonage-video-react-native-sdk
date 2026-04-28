52c69c0576581c7a538052a75944bda89aece599
Jose Antonio Olivera Ortega <joseantonio.oliveraortega@vonage.com>
2026-04-28 14:15:33 +0200

    Revert "fix: [VIDCS-4616] Fix OpentokReactNative-Swift.h import path"
    This reverts commit 99f33aace9c985ff60e98cff538a3f8b46fad3f8.

--- ios/OpentokReactNative.mm
+++ ios/OpentokReactNative.mm
@@ -1,10 +1,10 @@
 #import <Foundation/Foundation.h>
 #import <OpentokReactNative/RNOpentokReactNativeSpec.h>
 #if __has_include(<OpentokReactNative/OpentokReactNative-Swift.h>)
+#import <OpentokReactNative/OpentokReactNative-Swift.h>
+#else
 #import <OpentokReactNative-Swift.h>
-// #import <OpentokReactNative/OpentokReactNative-Swift.h>
-// #else
-// #endif
+#endif
 
 typedef JS::NativeOpentok::SessionOptions RN_SessionOptions;
 
